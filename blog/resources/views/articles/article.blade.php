d83870d8736e958517c5a0ad661df4be6af3813c
rodleyorosa <rodleyorosa@gmail.com>
2022-11-25 13:42:26 +0100

    comment structure created

--- blog/resources/views/articles/article.blade.php
+++ blog/resources/views/articles/article.blade.php
@@ -32,7 +32,17 @@
     <h2 class="title blue-moderate my-5">COMMENT SECTION</h2>
     @foreach ($comments as $comment)
     <div class="bg-white p-4 mt-1 rounded-sm shadow-sm border-2">
-        {{$comment->comments}}
+        <div class="flex">
+            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+                <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+            </svg>
+            <p class="username">Da implementare</p>
+            <p class="created-at">{{$comment->created_at->format('F d, Y')}}</p>
+        </div>
+        <div class="comment">
+            {{$comment->comments}}
+        </div>
     </div>
     @endforeach
 </div>
