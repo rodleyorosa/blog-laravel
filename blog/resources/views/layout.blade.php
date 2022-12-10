b5fbfa91612741da5476d1711c477a7f50cd5ee7
rodleyorosa <rodleyorosa@gmail.com>
2022-12-10 15:10:34 +0100

    changed font

--- blog/resources/views/layout.blade.php
+++ blog/resources/views/layout.blade.php
@@ -7,6 +7,7 @@
   <title>@yield('title')</title>
   <script src="https://cdn.tailwindcss.com"></script>
   <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Lato&display=swap" rel="stylesheet">
 </head>
 <body>
   
