6598849bb271061ae84fa3d036ccc236f8ce9297
rodleyorosa <rodleyorosa@gmail.com>
2022-12-10 16:21:54 +0100

    added laravel logo

--- blog/resources/views/layout.blade.php
+++ blog/resources/views/layout.blade.php
@@ -32,11 +32,8 @@
       <div class="md:flex">
         <div class="bg-white h-fit mx-auto rounded shadow-md sm:max-w-lg mb-4 md:w-64 md:mr-4">
           <div class="p-4">
-              <div class="mb-6">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-person-circle block m-auto" viewBox="0 0 16 16">
-                      <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-                      <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
-                  </svg>
+              <div class="mb-4">
+                <img class="w-3/4 m-auto" src="{{ asset('images/logo_laravel.png') }}" alt="description of myimage">
               </div>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi labore vitae minus reprehenderit iste molestias vel, fuga debitis explicabo commodi veritatis. Error, dolor excepturi non amet in aperiam nulla reprehenderit.</p>
               <hr class="my-4">
